fa1ad4591bfc75adf1cd230c18bb67c0fb0464db
Ayewww <153438832+Ayewww@users.noreply.github.com>
2023-12-11 17:54:00 +0900

    arduino

--- ParkingProjectArduino/ParkingProjectArduino.ino
+++ ParkingProjectArduino/ParkingProjectArduino.ino
@@ -1,10 +1,9 @@
 #include <ArduinoBearSSL.h>
 #include <ArduinoECCX08.h>
 #include <ArduinoMqttClient.h>
-#include <WiFiNINA.h> // Change to #include <WiFi101.h> for MKR1000
+#include <WiFiNINA.h> // MKR1000를 위해 #include <WiFi101.h>로 변경 가능
 
 #include "arduino_secrets.h"
-
 
 #define LED_1_PIN 5
 
@@ -13,29 +12,27 @@
 #include <Servo.h>
 #include <Wire.h>
 
-/////// Enter your sensitive data in arduino_secrets.h
-const char ssid[] = SECRET_SSID;
-const char pass[] = SECRET_PASS;
-const char broker[] = SECRET_BROKER;
-const char *certificate = SECRET_CERTIFICATE;
+/////// 민감한 데이터는 arduino_secrets.h에 입력합니다.
+const char ssid[] = SECRET_SSID;           // WiFi SSID
+const char pass[] = SECRET_PASS;           // WiFi 비밀번호
+const char broker[] = SECRET_BROKER;       // MQTT 브로커 주소
+const char *certificate = SECRET_CERTIFICATE; // 안전한 MQTT를 위한 SSL 인증서
 
-WiFiClient wifiClient;            // Used for the TCP socket connection
-BearSSLClient sslClient(wifiClient); // Used for SSL/TLS connection, integrates with ECC508
+WiFiClient wifiClient;             // TCP 소켓 연결용
+BearSSLClient sslClient(wifiClient); // ECC508과 통합되는 SSL/TLS 연결용
 MqttClient mqttClient(sslClient);
 
-unsigned long lastMillis = 0;
+unsigned long lastMillis = 0; // 주기적인 MQTT 메시지를 위한 시간 추적 변수
 
-Servo servo;
-int time = 0;
-int fee = 3000;
-int light;
-
+Servo servo; // 서보 모터 인스턴스
+int time = 0; // 시간 추적 변수
+int fee = 3000; // 요금 변수
+int light; // 빛의 세기 저장 변수
 
 void setup()
 {
   Serial.begin(115200);
   while (!Serial);
-
 
   if (!ECCX08.begin())
   {
@@ -48,27 +45,29 @@ void setup()
 
   mqttClient.onMessage(onMessageReceived);
 
-  servo.attach(8); // Assuming the servo is connected to pin 8
+  servo.attach(8); // 서보 모터가 핀 8에 연결되어 있다고 가정
 }
 
 void loop()
 {
-  light= analogRead(A1);
+  light = analogRead(A1);
+
+  // WiFi가 연결되어 있지 않으면 연결을 시도합니다.
   if (WiFi.status() != WL_CONNECTED)
   {
     connectWiFi();
   }
 
+  // MQTT 클라이언트가 연결되어 있지 않으면 연결을 시도합니다.
   if (!mqttClient.connected())
   {
-    // MQTT client is disconnected, connect
     connectMQTT();
   }
 
-  // Poll for new MQTT messages and send keep-alives
+  // 새로운 MQTT 메시지를 폴링하고 keep-alive를 전송합니다.
   mqttClient.poll();
 
-  // Publish a message roughly every 5 seconds.
+  // 대략 5초마다 메시지를 발행합니다.
   if (millis() - lastMillis > 5000)
   {
     lastMillis = millis();
@@ -77,8 +76,8 @@ void loop()
     sendMessage(payload);
   }
 
-  // Servo control
-  if (light < 900 )
+  // 빛의 강도에 따른 서보 제어
+  if (light < 900)
   {
     servo.write(-30);
     time = 0;
@@ -97,12 +96,13 @@ void loop()
   delay(1000);
 }
 
+// 현재 시간을 WiFi 모듈에서 가져오는 함수
 unsigned long getTime()
 {
-  // Get the current time from the WiFi module
   return WiFi.getTime();
 }
 
+// WiFi에 연결하는 함수
 void connectWiFi()
 {
   Serial.print("Attempting to connect to SSID: ");
@@ -111,7 +111,7 @@ void connectWiFi()
 
   while (WiFi.begin(ssid, pass) != WL_CONNECTED)
   {
-    // Failed, retry
+    // 실패하면 재시도
     Serial.print(".");
     delay(5000);
   }
@@ -121,6 +121,7 @@ void connectWiFi()
   Serial.println();
 }
 
+// MQTT 브로커에 연결하는 함수
 void connectMQTT()
 {
   Serial.print("Attempting to MQTT broker: ");
@@ -129,7 +130,7 @@ void connectMQTT()
 
   while (!mqttClient.connect(broker, 8883))
   {
-    // Failed, retry
+    // 실패하면 재시도
     Serial.print(".");
     delay(5000);
   }
@@ -138,19 +139,18 @@ void connectMQTT()
   Serial.println("You're connected to the MQTT broker");
   Serial.println();
 
-  // Subscribe to a topic
+  // 특정 주제를 구독합니다.
   mqttClient.subscribe("$aws/things/MyMKRWiFi1010/shadow/update/delta");
 }
 
+// 디바이스 상태 페이로드를 작성하는 함수
 void getDeviceStatus(char *payload)
 {
-  // Read temperature as Celsius (the default)
-  //float t = dht.readTemperature();
-
-  // Make payload for the device update topic ($aws/things/MyMKRWiFi1010/shadow/update)
+  // 디바이스 업데이트 주제를 위한 페이로드 생성
   sprintf(payload, "{\"state\":{\"reported\":{\"light\":\"%d\",\"time\":\"%d\",\"fee\":\"%d\",\"servo\":\"%d\"}}}", light, time, fee, servo.read());
 }
 
+// MQTT 메시지를 보내는 함수
 void sendMessage(char *payload)
 {
   char TOPIC_NAME[] = "$aws/things/MyMKRWiFi1010/shadow/update";
@@ -162,26 +162,28 @@ void sendMessage(char *payload)
   mqttClient.endMessage();
 }
 
+// MQTT 메시지를 받았을 때의 콜백 함수
 void onMessageReceived(int messageSize)
 {
-  // We received a message, print out the topic and contents
   Serial.print("Received a message with topic '");
   Serial.print(mqttClient.messageTopic());
   Serial.print("', length ");
   Serial.print(messageSize);
   Serial.println(" bytes:");
 
-  // Store the message received to the buffer
+  // 받은 메시지를 버퍼에 저장
   char buffer[512];
   int count = 0;
   while (mqttClient.available())
   {
     buffer[count++] = (char)mqttClient.read();
   }
-  buffer[count] = '\0'; // Buffer's last character is a null character
+  buffer[count] = '\0'; // 버퍼의 마지막 문자는 널 문자
+
   Serial.println(buffer);
   Serial.println();
 
+  // 받은 JSON 메시지를 파싱
   DynamicJsonDocument doc(1024);
   deserializeJson(doc, buffer);
   JsonObject root = doc.as<JsonObject>();
@@ -191,12 +193,11 @@ void onMessageReceived(int messageSize)
 
   char payload[512];
 
+  // 받은 빛 값에 따라 동작
   if (receivedLight == 0)
   {
-    // Move the servo motor
+    // 서보 모터를 움직임
     servo.write(-30);
     delay(10000);
   }
 }
-
-
